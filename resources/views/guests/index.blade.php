13f169dd73c73b6e02434490968e0f556ca1c62c
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-06-22 20:24:00 +0200

    inserimento img guest index

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -6,6 +6,7 @@
         <div class="col-2 d-flex my-2">
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
+                    <img src="{{asset('storage/' . $elem->path)}}" alt="">
                   <h5 class="card-title">{{$elem->title}}</h5>
                   <p class="card-text">{{$elem->description}}</p>
                 <div class="btn btn-primary">{{$elem->lang}}</div>
